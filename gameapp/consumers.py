64206068e0c100db47f380c6c52f686e7c8c871f
Willem Nieuwenhuis <ruan.nieu@gmail.com>
2025-11-19 15:38:07 +0200

    Added Channel Layer

--- gameapp/consumers.py
+++ gameapp/consumers.py
@@ -1,22 +1,42 @@
 # chat/consumers.py
 import json
 
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.room_group_name = "game"
+
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
+
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
 
+    # Receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         x = text_data_json["x"]
         y = text_data_json["y"]
 
-        self.send(text_data=json.dumps({
-            "x":x,
-            "y":y
-            }))
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name, {"type": "chat.message", "x": x, "y":y}
+        )
+
+    # Receive message from room group
+    def chat_message(self, event):
+        x = event["x"]
+        y = event["y"]
+
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({"x": x, "y":y}))
--- snowbound/settings.py
+++ snowbound/settings.py
@@ -129,3 +129,11 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ASGI_APPLICATION = "snowbound.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
